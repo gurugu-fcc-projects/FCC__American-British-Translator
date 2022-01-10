48a810a3da60288d28f7140ea80708b2a509fec8
GuRuGu <peter.k.nets@gmail.com>
2022-01-10 11:52:27 +0200

    Handle titles when translating to British

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,45 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
+  translate(text, locale) {
+    const preparedText = text.trim().split(" ");
+    console.log(preparedText);
+    const processedText = [];
 
+    for (const [i, word] of preparedText.entries()) {
+      let processedWord = "";
+
+      if (locale === "american-to-british") {
+        /* Handle titles */
+        if (i !== preparedText.length - 1 && word.endsWith(".")) {
+          processedWord = americanToBritishTitles[word.toLowerCase()]
+            ? word.slice(0, -1)
+            : word;
+        } else {
+          processedWord = word;
+        }
+        // if (americanToBritishSpelling.hasOwnProperty(word)) {
+        //   processedText.push(
+        //     `<span class="highlight">${americanToBritishSpelling.word}</span>`
+        //   );
+        // } else if (americanToBritishTitles.hasOwnProperty(word)) {
+        //   processedText.push(
+        //     `<span class="highlight">${americanToBritishTitles.word}</span>`
+        //   );
+        // } else {
+        //   processedText.push(word);
+        // }
+      } else {
+      }
+
+      processedText.push(processedWord);
+    }
+
+    return processedText.join(" ");
+  }
 }
 
 module.exports = Translator;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,8 +1,20 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
+const translator = new Translator();
 
-suite('Unit Tests', () => {
+suite("Unit Tests", () => {
+  test("Translate 'Mangoes are my favorite fruit.' to British English", (done) => {
+    const input = {
+      text: "No Mr. Bond, I expect you to die.",
+      locale: "american-to-british",
+    };
 
+    const result = translator.translate(input.text, input.locale);
+
+    console.log(result);
+
+    done();
+  });
 });
