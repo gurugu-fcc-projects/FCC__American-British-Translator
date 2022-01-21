0eec69c43c9228b9920183de66b3d0884f3c7760
GuRuGu <peter.k.nets@gmail.com>
2022-01-21 16:37:27 +0200

    Text general translation operation

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,24 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    if (!req.body.text || !req.body.locale) {
+      return res.json({ error: "Required field(s) missing" });
+    }
+
+    console.log(req.body.text);
+    if (req.body.text.length === 0) {
+      return res.json({ error: "No text to translate" });
+    }
+
+    const { text, locale } = req.body;
+
+    const translation = translator.translate(text, locale);
+
+    res.json({ text, translation });
+  });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,12 +1,31 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server.js');
+const server = require("../server.js");
 
 chai.use(chaiHttp);
 
-let Translator = require('../components/translator.js');
+let Translator = require("../components/translator.js");
 
-suite('Functional Tests', () => {
-
+suite("Functional Tests", () => {
+  test("Translation with text and locale fields: POST request to /api/translate", (done) => {
+    chai
+      .request(server)
+      .post("/api/translate")
+      .send({
+        text: "Mangoes are my favorite fruit.",
+        locale: "american-to-british",
+      })
+      .end((err, res) => {
+        assert.isObject(res);
+        assert.property(res.body, "text");
+        assert.property(res.body, "translation");
+        assert.equal(res.body.text, "Mangoes are my favorite fruit.");
+        assert.equal(
+          res.body.translation,
+          'Mangoes are my <span class="highlight">favourite</span> fruit.'
+        );
+        done();
+      });
+  });
 });
